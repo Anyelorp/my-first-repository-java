298082c10b68a34ac5b390c78b6b158f80aa53be
Anyelorp <14886492@sernati.pe>
2023-03-25 10:32:36 -0500

    ejersicio5

--- Mysecondproject/src/mysecondproject/Ejersicio3.java
+++ Mysecondproject/src/mysecondproject/Ejersicio3.java
@@ -1,13 +1,18 @@
 package ejersicio.pkg2;
-
+import java.util.Scaner;
+import java.util.Scanner;
 public class Ejersicio2 {
     
     public static void main(String[] args) {
-
-         int i;
-    for (i = 1; i <= 999; i++){
-        if (i % 2 == 1){
-            System.out.println(i);
+        Scanner n = new Scanner (System.in);
+        System.out.println("ingrese el primer numero: ");
+        int n1 = n.nextInt();
+        System.out.println("ingrese el segundo numero: ");
+        int n2 = n.nextInt();
+        System.out.println("los numeros impares de " + n1 + "a" + n2 + "son:");
+        for (int i= 1; n1 <= n2; i++){
+            if (i % 2 == 1){
+                System.out.println(i);
         }
     }                
     }      
