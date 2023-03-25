5028415e97b777abf729fee32da70dd80ba04328
Anyelorp <14886492@sernati.pe>
2023-03-25 09:53:14 -0500

    ejersicio4

---
+++ Mysecondproject/src/mysecondproject/Ejersicio3.java
@@ -0,0 +1,14 @@
+package ejersicio.pkg2;
+
+public class Ejersicio2 {
+    
+    public static void main(String[] args) {
+
+         int i;
+    for (i = 1; i <= 999; i++){
+        if (i % 2 == 1){
+            System.out.println(i);
+        }
+    }                
+    }      
+}  
