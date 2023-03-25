64daa1736d1b3e44b40065ee57c6e391946cf404
Anyelorp <14886492@sernati.pe>
2023-03-25 09:21:00 -0500

    Hello

---
+++ Mysecondproject/src/mysecondproject/CodigoDeProgramacio.java
@@ -0,0 +1,12 @@
+package CodigoDeProgramacio;
+public class CodigoDeProgramacio {
+    public static void main(String[] args) {
+        System.out.println("Morning, Morning, Morning");
+        System.out.println("     Hello, Have a good day");
+        System.out.println("             oh! Thanks you");
+        System.out.println("             you are great");
+        System.out.println("         this is my pleaureg");
+        System.out.println("No,...you are welcome");
+
+    }
+}
